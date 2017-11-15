a8d680149565411c88b3d45d95e59ab0f80540a7
brouxhaha <andy.debroux@gmail.com>
2017-11-14 21:38:51 -0800

    added export functionality and css for buttons

--- js/main.js
+++ js/main.js
@@ -10,17 +10,16 @@ var LiteBrite = {
 		this.pegs;
 		this.saveArray = [];
 
-		
 		this.createBoard();
 		this.createColorPegs();
 		this.createButtons();
 		this.clickHandler();
-
 	},
 
 	createBoard: function(){
 		let documentFragment = document.createDocumentFragment(),
-			boardDiv = document.createElement('div');
+			boardDiv = document.createElement('div'),
+			index = 0;
 
 		boardDiv.className = 'board';
 
@@ -33,10 +32,12 @@ var LiteBrite = {
 			for(var j=0; j < this.numColumns; j++){
 				const hole = document.createElement('div');
 
-				hole.className = this.startingBoard ? this.startingBoard[i + j] : 'hole';
+				hole.className = this.startingBoard ? this.startingBoard[index] : 'hole';
 				hole.setAttribute('id', 'hole_row' + i + '_col' + j);
-					
+				
 				docFrag.appendChild(hole);
+
+				index++;
 			}
 
 			rowDiv.appendChild(docFrag);
@@ -77,27 +78,32 @@ var LiteBrite = {
 
 	createButtons: function(){
 		const buttons = document.createElement('div'),
-			  resetButton = document.createElement('button'),
 			  saveButton = document.createElement('button'),
-			  loadButton = document.createElement('button');
+			  loadButton = document.createElement('button'),
+			  exportButton = document.createElement('button'),
+			  resetButton = document.createElement('button');
 
 		buttons.className = 'buttons';
-		resetButton.className = 'button button--reset';
-		resetButton.innerText = 'Reset Board';
 		saveButton.className = 'button button--save';
 		saveButton.innerText = 'Save Board';
 		loadButton.className = 'button button--load';
 		loadButton.innerText = 'Load Board';
+		exportButton.className = 'button button--export';
+		exportButton.innerText = 'Export Board Data';
+		resetButton.className = 'button button--reset';
+		resetButton.innerText = 'Reset Board';
 
 		this.mainDiv.appendChild(buttons);
 		this.buttonDiv = document.querySelector('.buttons');
-		this.buttonDiv.appendChild(resetButton);
 		this.buttonDiv.appendChild(saveButton);
 		this.buttonDiv.appendChild(loadButton);
+		this.buttonDiv.appendChild(exportButton);
+		this.buttonDiv.appendChild(resetButton);
 
-		this.resetBtn = this.buttonDiv.querySelector('.button--reset');
 		this.saveBtn = this.buttonDiv.querySelector('.button--save');
 		this.loadBtn = this.buttonDiv.querySelector('.button--load');
+		this.exportBtn = this.buttonDiv.querySelector('.button--export');
+		this.resetBtn = this.buttonDiv.querySelector('.button--reset');
 	},
 
 	selectColor: function(color){
@@ -112,12 +118,11 @@ var LiteBrite = {
 	},
 
 	changeHoleColor: function(hole){
-		//red, blue, orange, white (clear/colorless), green, yellow, pink, violet (purple)
+		hole.className = 'hole';
+		
 		if(this.currentColor){
 			hole.classList.add(this.currentColor);
-		} else {
-			hole.className = 'hole';
-		}
+		} 
 	},
 
 	clearBoard: function(){
@@ -148,8 +153,14 @@ var LiteBrite = {
 		});
 	},
 
-	exportBoardArray(){
+	exportBoardData(){
+		let saveArray = [];
 
+		this.holes.forEach(hole => {
+			saveArray.push("'" + hole.className + "'");
+		});
+
+		alert('Copy the below code. You can enter it as the last parameter to persist from the start.\n\n' + '[' + saveArray + ']');
 	},
 
 	clickHandler: function(){
@@ -158,11 +169,12 @@ var LiteBrite = {
 		this.resetBtn.addEventListener('click', this.clearBoard.bind(this));
 		this.saveBtn.addEventListener('click', this.saveBoard.bind(this));
 		this.loadBtn.addEventListener('click', this.loadBoard.bind(this));
+		this.exportBtn.addEventListener('click', this.exportBoardData.bind(this));
 	}
 };
 
 (function(){
 	var liteBrite = Object.create(LiteBrite);
-	liteBrite.init('.lite-brite');
+	liteBrite.init('.lite-brite', ['hole red','hole red','hole red','hole red','hole red','hole','hole','hole orange','hole orange','hole orange','hole orange','hole','hole','hole yellow','hole','hole','hole','hole yellow','hole','hole green','hole green','hole green','hole green','hole green','hole','hole blue','hole blue','hole blue','hole blue','hole blue','hole pink','hole pink','hole pink','hole pink','hole pink','hole purple','hole purple','hole purple','hole purple','hole white','hole white','hole white','hole white','hole','hole red','hole','hole','hole','hole','hole','hole','hole orange','hole','hole','hole','hole orange','hole','hole yellow','hole yellow','hole','hole','hole yellow','hole','hole green','hole','hole','hole','hole','hole','hole','hole blue','hole','hole','hole','hole blue','hole pink','hole','hole','hole','hole pink','hole','hole purple','hole','hole white','hole','hole','hole','hole','hole red','hole','hole','hole','hole','hole','hole orange','hole','hole','hole','hole','hole orange','hole','hole yellow','hole yellow','hole','hole','hole yellow','hole','hole green','hole','hole','hole','hole','hole','hole blue','hole','hole','hole','hole blue','hole pink','hole','hole','hole','hole pink','hole','hole','hole purple','hole white','hole','hole','hole','hole','hole','hole red','hole','hole','hole','hole','hole','hole','hole orange','hole','hole','hole','hole orange','hole','hole yellow','hole','hole yellow','hole','hole yellow','hole','hole green','hole','hole','hole','hole','hole','hole','hole blue','hole blue','hole blue','hole blue','hole','hole pink','hole','hole','hole','hole pink','hole','hole purple','hole','hole white','hole white','hole white','hole white','hole','hole red','hole','hole','hole','hole','hole','hole orange','hole','hole','hole','hole','hole orange','hole','hole yellow','hole','hole yellow','hole','hole yellow','hole','hole green','hole','hole green','hole green','hole green','hole','hole blue','hole blue','hole blue','hole','hole','hole pink','hole pink','hole pink','hole pink','hole pink','hole','hole','hole purple','hole','hole','hole','hole','hole white','hole','hole red','hole','hole','hole','hole','hole','hole','hole orange','hole','hole','hole','hole orange','hole','hole yellow','hole','hole','hole yellow','hole yellow','hole','hole green','hole','hole','hole','hole','hole green','hole','hole blue','hole','hole blue','hole','hole','hole pink','hole','hole','hole','hole pink','hole','hole purple','hole','hole','hole','hole','hole','hole white','hole red','hole','hole','hole','hole','hole','hole orange','hole','hole','hole','hole','hole orange','hole','hole yellow','hole','hole','hole yellow','hole yellow','hole','hole green','hole','hole','hole','hole green','hole','hole blue','hole','hole','hole blue','hole','hole pink','hole','hole','hole','hole pink','hole','hole','hole purple','hole','hole','hole','hole','hole white','hole','hole red','hole red','hole red','hole red','hole red','hole red','hole','hole orange','hole orange','hole orange','hole orange','hole orange','hole','hole yellow','hole','hole','hole','hole yellow','hole','hole green','hole green','hole green','hole green','hole green','hole green','hole','hole blue','hole','hole','hole','hole blue','hole pink','hole','hole','hole','hole pink','hole','hole purple','hole','hole white','hole white','hole white','hole white','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole white','hole white','hole white','hole white','hole white','hole','hole purple','hole','hole','hole','hole','hole purple','hole','hole','hole pink','hole pink','hole pink','hole pink','hole','hole blue','hole blue','hole blue','hole blue','hole','hole green','hole green','hole green','hole green','hole green','hole green','hole','hole yellow','hole yellow','hole yellow','hole yellow','hole yellow','hole yellow','hole','hole red','hole red','hole red','hole red','hole','hole pink','hole white','hole','hole','hole','hole','hole white','hole','hole purple','hole','hole','hole','hole purple','hole','hole','hole pink','hole','hole','hole','hole','hole blue','hole','hole','hole','hole','hole','hole','hole','hole green','hole','hole','hole','hole','hole yellow','hole','hole','hole','hole','hole','hole red','hole','hole','hole','hole','hole pink','hole white','hole','hole','hole','hole white','hole','hole purple','hole','hole','hole','hole','hole purple','hole','hole pink','hole','hole','hole','hole','hole blue','hole','hole','hole','hole','hole','hole','hole','hole green','hole','hole','hole','hole','hole yellow','hole','hole','hole','hole','hole','hole red','hole','hole','hole','hole','hole','hole pink','hole white','hole','hole','hole','hole','hole white','hole','hole purple','hole','hole','hole','hole purple','hole','hole','hole pink','hole pink','hole pink','hole pink','hole','hole blue','hole blue','hole blue','hole blue','hole','hole','hole','hole','hole green','hole','hole','hole','hole','hole yellow','hole','hole','hole','hole','hole','hole red','hole red','hole red','hole red','hole','hole pink','hole white','hole white','hole white','hole white','hole white','hole','hole purple','hole','hole','hole','hole','hole purple','hole','hole','hole','hole','hole','hole pink','hole','hole','hole','hole','hole blue','hole','hole','hole','hole green','hole','hole','hole','hole','hole','hole yellow','hole yellow','hole yellow','hole','hole','hole','hole','hole','hole','hole red','hole','hole pink','hole white','hole','hole','hole','hole','hole white','hole','hole purple','hole','hole','hole','hole purple','hole','hole','hole','hole','hole','hole','hole pink','hole','hole','hole','hole','hole blue','hole','hole','hole','hole green','hole','hole','hole','hole','hole yellow','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole red','hole pink','hole white','hole','hole','hole','hole white','hole','hole purple','hole','hole','hole','hole','hole purple','hole','hole','hole','hole','hole','hole pink','hole','hole','hole','hole','hole blue','hole','hole','hole','hole green','hole','hole','hole','hole','hole yellow','hole','hole','hole','hole','hole','hole','hole','hole','hole','hole red','hole','hole','hole white','hole','hole','hole','hole','hole white','hole','hole purple','hole purple','hole purple','hole purple','hole purple','hole','hole','hole pink','hole pink','hole pink','hole pink','hole','hole blue','hole blue','hole blue','hole blue','hole','hole green','hole green','hole green','hole green','hole green','hole green','hole','hole','hole yellow','hole yellow','hole yellow','hole yellow','hole yellow','hole yellow','hole red','hole red','hole red','hole red','hole','hole pink']);
 })();
 
